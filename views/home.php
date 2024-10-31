f44c3d6c922f66aeaa46f44b63164ae805698542
brunadz <124889935+brunadz@users.noreply.github.com>
2024-10-31 06:13:06 -0300

    Add files via upload

---
+++ views/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho Final TPA Grupo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 style="padding-left: 600px;">Bem vindo ao seu sistema de Plano de Saúde</h1>
+    <nav>
+        <a href="/project_mvc-main-tpaatividade/logout.php"><button style="position: absolute; right: 60px; top: 10px;" type="button" class="btn btn-dark">Login</button>
+        <ul>
+            <div class="d-grid gap-2 col-1 mx-auto" style="position: relative; top: 200px; width: 100px;">
+                <a href="/project_mvc-main-tpaatividade/index.php?action=prestador" style="position: relative"><button class="btn btn-primary" type="button">Gerenciar prestadores</button></a>
+                <a href="/project_mvc-main-tpaatividade/index.php?action=procedimento"><button class="btn btn-primary" type="button">Gerenciar procedimentos</button></a>
+                <a href="/project_mvc-main-tpaatividade/index.php?action=beneficiario"><button class="btn btn-primary" type="button">Gerenciar beneficiários</button></a>
+            </div>
+        </ul>
+    </nav>
+    <div class="card text-center " style="position: absolute; top: 700px; width: 100%; height: 225px; background-color: #87CEFA;">
+        <div class="card-body" style="position: relative; right: 600px;">
+            <h5 class="card-title">Portfólios:</h5>
+            <p class="card-text"><a href="https://github.com/brunadz">Bruna Damasceno</a></p>
+            <p class="card-text"><a href="https://github.com/Henrique1858">Henrique Petronio</a></p>
+            <p class="card-text"><a href="https://github.com/LucasMP17">Lucas Marques</a></p>
+            <p class="card-text"><a href="https://github.com/Itux11">Italo Ribeiro</a></p>
+            <h5 class="card-title" style="position: absolute; left: 1450px; width: 1000px; bottom: 130px;">Um plano de saúde é um sistema de assistência médica que oferece acesso a consultas, exames, internações e diversos serviços de saúde. Com a contratação de um plano, você conta com uma rede de profissionais e estabelecimentos credenciados para cuidar da sua saúde e da sua família de forma acessível e segura.</h5>
+            <h5 class="card-title" style="position: absolute; left: 1450px; width: 1000px; bottom: 30px;">Nós trabalhamos para que você tenha o melhor atendimento, com cobertura ampla e suporte em todas as etapas da sua jornada de saúde. Consulte nossos planos e encontre o que melhor se adapta às suas necessidades.</h5>
+        </div>
+    </div>
+
+</body>
+
+</html>
